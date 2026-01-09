c6453ed77570d563ef979e26ef9e48a430f2f8e3
NotArchon <notarchon@gmail.com>
2026-01-09 16:21:36 -0700

    wrangler.toml

--- index.js
+++
@@ -1,7 +0,0 @@
-export default {
-    async fetch(request, env, ctx) {
-        // You can view your logs in the Observability dashboard
-        console.info({ message: 'From GitHub' });
-        return new Response('Hello World From GitHub!');
-    }
-};
